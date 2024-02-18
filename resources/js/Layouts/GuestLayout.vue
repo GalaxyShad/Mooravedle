76f48299bb3b59db3b18e6d7602daaed832963be
GalaxyShad <vadim.kadyrin@mail.ru>
2024-02-18 12:57:01 +0500

    ✨prettier config

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -1,20 +1,24 @@
 <script setup>
-import ApplicationLogo from '@/Components/ApplicationLogo.vue';
-import { Link } from '@inertiajs/vue3';
+import ApplicationLogo from '@/Components/ApplicationLogo.vue'
+import { Link } from '@inertiajs/vue3'
 </script>
 
 <template>
-    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-        <div>
-            <Link href="/">
-                <ApplicationLogo class="w-20 h-20 fill-current text-gray-500" />
-            </Link>
-        </div>
+  <div
+    class="bg-slate-200 min-h-screen flex flex-col sm:justify-center items-center"
+  >
+    <div
+      class="flex flex-row items-center bg-white mt-6 px-12 py-12 border overflow-hidden sm:rounded-lg gap-16"
+    >
+      <div>
+        <Link href="/">
+          <ApplicationLogo class="w-64 fill-current text-gray-500" />
+        </Link>
+      </div>
 
-        <div
-            class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg"
-        >
-            <slot />
-        </div>
+      <div class="w-full sm:max-w-md">
+        <slot />
+      </div>
     </div>
+  </div>
 </template>
